95f7ab5ab407c4eae1b2deee2a98743701c43f39
Sean Yu <sean.xiaochen.yu@gmail.com>
2020-10-21 18:50:40 -0400

    updated character_of_json and from_json to take in a string for which json to parse

--- character.ml
+++ character.ml
@@ -1,10 +1,10 @@
 open Yojson.Basic.Util
 type name = string
-type four_type = Fire | Earth | Water | Air
+type element = Fire | Earth | Water | Air | Avatar | Bruh
 
 type move = {
-  mname : name;
-  mtype : four_type;
+  m_name : name;
+  m_element : element;
 
 }
 
@@ -16,10 +16,10 @@ type stats = {
 }
 
 type character = {
-  cname : name;
+  c_name : name;
   description : string;
   isplayer: bool;
-  ctype : four_type;
+  c_element : element;
   stats : stats;
   moves : move list
 }
@@ -27,16 +27,33 @@ type character = {
 type t = {
   characters : character list;
 }
-(*)
+
+let to_element = function
+  | "Air" -> Air
+  | "Fire" -> Fire 
+  | "Earth" -> Earth
+  | "Water" -> Water 
+  | "Avatar" -> Avatar
+  | _ -> Bruh
+
+let stats_of_json =
+  failwith "to do"
+
+let moves_of_json =
+  failwith "to do"
+
 let character_of_json j = {
-  cname = j |> member "name" |> to_string;
+  c_name = j |> member "name" |> to_string;
   description = j |> member "description" |> to_string;
   isplayer = j |> member "isplayer" |> to_bool;
+  c_element = j |> member "element" |> to_string |> to_element;
+  stats = j |> member "stats" |> stats_of_json;
+  moves = j |> member "moves" |> to_list |> List.map moves_of_json
 
 }
 
 let from_json file_name = 
-  json = Yojson.Basic.from_file (file_name ^ ".json")
+  let json = Yojson.Basic.from_file (file_name ^ ".json") in
   {
     characters = json |> member "characters" |> to_list |> List.map character_of_json;
-  }*)
+  }
