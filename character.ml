9d0081d5ad9bb49a9508bab4e31ed14893cee758
Sean Yu <sean.xiaochen.yu@gmail.com>
2020-10-22 23:47:57 -0400

    finished character.ml and updated character.mli

--- character.ml
+++ character.ml
@@ -1,12 +1,19 @@
 open Yojson.Basic.Util
 type name = string
 type element = Fire | Earth | Water | Air | Avatar | Bruh
+exception UnknownCharacter of name
 
 type move = {
+  id: int;
+  is_super: bool;
   m_name : name;
   m_element : element;
-
+  m_description: string;
+  damage: float;
+  pp: int
 }
+
+exception UnknownMove of int
 
 type stats = {
   health: float;
@@ -17,7 +24,7 @@ type stats = {
 
 type character = {
   c_name : name;
-  description : string;
+  c_description : string;
   isplayer: bool;
   c_element : element;
   stats : stats;
@@ -36,19 +43,31 @@ let to_element = function
   | "Avatar" -> Avatar
   | _ -> Bruh
 
-let stats_of_json =
-  failwith "to do"
+let stats_of_json j = {
+  health = j |> member "health" |> to_float;
+  power = j |> member "power" |> to_float;
+  speed = j |> member "speed" |> to_float;
+  evasiveness = j |> member "evasiveness" |> to_float;
+}
+  
 
-let moves_of_json =
-  failwith "to do"
+let move_of_json j = {
+  id = j |> member "id" |> to_int;
+  m_name = j |> member "name" |> to_string;
+  is_super = j |> member "issuper" |> to_bool;
+  m_element = j |> member "element" |> to_string |> to_element;
+  m_description = j |> member "description" |> to_string;
+  damage = j |> member "damage" |> to_float;
+  pp = j |> member "pp" |> to_int;
+}
 
 let character_of_json j = {
   c_name = j |> member "name" |> to_string;
-  description = j |> member "description" |> to_string;
+  c_description = j |> member "description" |> to_string;
   isplayer = j |> member "isplayer" |> to_bool;
   c_element = j |> member "element" |> to_string |> to_element;
   stats = j |> member "stats" |> stats_of_json;
-  moves = j |> member "moves" |> to_list |> List.map moves_of_json
+  moves = j |> member "moves" |> to_list |> List.map move_of_json
 }
 
 let from_json file_name = 
@@ -60,6 +79,40 @@ let from_json file_name =
     |> List.map character_of_json
   }
 
-(* let rec get_names_helper 
+let get_names (ep : t) : name list =
+  let rec helper (c_list : character list) acc = 
+    match c_list with 
+    | c :: t -> helper t (c.c_name :: acc)
+    | _ -> acc
+  in
+  List.rev (helper ep.characters [])
 
-let get_names t = get_names_helper [] t.rooms  *)
+(* helper for getting a character record *)
+let rec get_c_by_name c_list name = 
+  match c_list with 
+  | c :: t -> if c.c_name = name then c else get_c_by_name t name
+  | [] -> raise (UnknownCharacter name)
+
+let get_c_element ep name = 
+  let character = get_c_by_name ep.characters name in
+  character.c_element
+
+let get_stats ep name = 
+  let character = get_c_by_name ep.characters name in
+  character.stats
+
+let get_c_description ep name = 
+  let character = get_c_by_name ep.characters name in
+  character.c_description
+
+let get_moves ep name =
+  let character = get_c_by_name ep.characters name in
+  character.moves
+
+let get_move_by_id ep name id : move =
+  let rec helper move_list id = 
+    match move_list with 
+    | m :: t -> if m.id = id then m else helper t id
+    | _ -> raise (UnknownMove id)
+  in
+  helper (get_moves ep name) id
--- character.mli
+++ character.mli
@@ -11,32 +11,53 @@ type t
 
 type name = string
 
-type stats
+type stats = {
+   health: float;
+   power: float;
+   speed: float;
+   evasiveness: float
+ }
 
 type element = Fire | Earth | Water | Air | Avatar | Bruh
 
 type description = string
 
-type moves
+(* all characters at most have 4 moves**)
+type move = {
+  id: int;
+  m_name : name;
+  is_super: bool;
+  m_element : element;
+  m_description: string;
+  damage: float;
+  pp: int
+}
+
+(* UnknownCharacter is thrown when attempting to call on unknown character *)
+exception UnknownCharacter of name
+
+(* UnknownMove is thrown when attempting to call on unknown move *)
+exception UnknownMove of int
 
 (* [from_json] takes input of the JSON file name to load (DO NOT INCLUDE 
 '.json') and returns an instance of t*)
 val from_json : string -> t
 
-(*[get_names a] also guarantees that the first index of the returned name list is 
+(*[get_names] also guarantees that the first index of the returned name list is 
 always the player character e.g. Aang*)
 val get_names : t -> name list
 
-(* [get_element a] is the type of character,*)
-val get_element : t -> name -> element
+(* [get_element] is the type of character,*)
+val get_c_element : t -> name -> element
 
-(* [get_stats a] are the stats for character. *)
+(* [get_stats] are the stats for character. *)
 val get_stats : t -> name -> stats
 
-(* [get_description a] is the description of character *)
-val get_description : t -> name -> description
+(* [get_description] is the description of character *)
+val get_c_description : t -> name -> description
 
-(* [get_moves a] returns a record representing the moveset of character.  (t)*)
-val get_moves : t -> name -> moves
+(* [get_moves] returns a list representing the moveset of character. *)
+val get_moves : t -> name -> move list
 
-
+(* [get_move_by_id] returns a record representing a move from a move id. *)
+val get_move_by_id : t -> move list -> move
